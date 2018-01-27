187cb1e4db778c97afc5d3c060ca0b006e176c9b
Evan Baker <3van90@gmail.com>
2018-01-27 14:47:17 -0500

    Add nodemon and npm start script

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(/\/((?!graphql).)*/, bodyParser.urlencoded({ extended: true }));
 app.use(/\/((?!graphql).)*/, bodyParser.json());
-app.use('/static', express.static(path.join(__dirname, 'Public')))
+app.use('/static', express.static(path.join(__dirname, '../Public')))
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true,
@@ -29,14 +29,7 @@ app.post('/', (req, res, next) => {
   });
 });
 
-server.get('/', (req, res, next) => {
-  let instruction = 'POST GraphQL queries to' + server.url + '. Sample query: {contributor (id: "1")}';
-  res.send(instruction);
-});
-
 //express-graphql will accept requests with 3 parameters: (query, variables, operationName)
-
-
 
 app.listen(4000, () => {
   console.log('Listening on port 4000')
