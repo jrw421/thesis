baeb9ccec50fd25eb08d2392a70b6cc493209521
CaioAR <cmourani12@yahoo.com>
2018-01-29 16:38:46 -0500

    nested querys working! items and events types finished

--- Server/SchemaGQL/schema.js
+++ Server/SchemaGQL/schema.js
@@ -1,10 +1,14 @@
 const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 
+
+const _ = require('lodash');
+
 const mutations = require('./mutations')
 const UserType = require('./types.js').UserType
 const EventType = require('./types.js').EventType
 const ItemType = require('./types.js').ItemType
+
 
 
 const RootQueryType = require('./root_query_type');
