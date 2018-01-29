6e2a438cefbe8d7dd176d053440bf47563a63866
Caio Araujo Roper <cararoper@gmail.com>
2018-01-29 15:36:58 -0500

    Add 3 mutations to GraphQL.

--- Server/SchemaGQL/root_query_type.js
+++ Server/SchemaGQL/root_query_type.js
@@ -12,18 +12,6 @@ const {
 } = graphql;
 
 
-
-var users = [
-  { id: 23, name: 'Bill', email: 'csfsd@sdfss.com', token: '234234', member_status: false},
-  { id: 28, name: 'Alice', email: 'csfsd@sdfsdfsss.com', token: '23sdf4234', member_status: false}
-]
-
-let events = [
-  { id: 22, userId: 23, name: 'my party', description: 'a party', date: '04-13-2019', location: '23 Pine Street'},
-  { id: 29, userId: 28, name: 'come on over', description: 'another party', date: '04-13-2019', location: '23 Pine Street'}
-]
-
-
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: () => ({
