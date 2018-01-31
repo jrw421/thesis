0148d314116fe87a953922c2df1a5231c78c5f39
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-01-31 10:46:17 -0500

    merging

--- Server/server.js
+++ Server/server.js
@@ -16,6 +16,67 @@ const passport = require('passport');
 
 const app = express();
 
+const flash = require('connect-flash');
+const util = require('util')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const $ = require('jquery');
+
+const app = express();
+
+
+//contacts///
+
+app.get('/contacts', function(req, res) {
+  function auth() {
+    var config = {
+      'client_id': '3van90@gmail.com',
+      'scope': 'https://www.google.com/m8/feeds'
+    };
+    gapi.auth.authorize(config, function() {
+      fetch(gapi.auth.getToken());
+    });
+  }
+
+  function fetch(token) {
+    $.ajax({
+      url: 'https://www.google.com/m8/feeds/contacts/default/full?alt=json',
+      dataType: 'jsonp',
+      data: token
+    }).done(function(data) {
+      console.log("data ", JSON.stringify(data));
+    });
+  }
+
+})
+/////////
+
+
+// auth //
+/////////
+passport.use(new GoogleStrategy({
+  clientID: '958835359621-ar0pkshcuaba693ki10vaq1cc1j6qtk8.apps.googleusercontent.com',
+  clientSecret: '4qDzcSsqkWieHEABXAf1XMpH',
+  callbackURL: 'http://localhost:4000/login/google/return',
+  passReqToCallback: true
+},
+function(request, accessToken, refreshToken, profile, done) {
+  // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  //   return cb(err, user);
+  // });
+  console.log('profile', profile)
+  process.nextTick(()=> {return done(null, profile)})
+}
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+>>>>>>> merging
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
@@ -50,6 +111,21 @@ app.get('/user', function(req, res) {
       });
   }
 });
+// Google Contacts //
+/////////////////////
+//
+// app.get('/emails', function(req, res){
+//   console.log('inside emails route')
+//   axios.get('https://www.google.com/m8/feeds/contacts/3van90@gmail.com/full')
+//       .then(data => {
+//         console.log('data', data)
+//         res.end(data)
+//       })
+//       .catch(error => {
+//         console.log('error', error)
+//         res.end(error)
+//       })
+// })
 
 // graphql //
 ////////////
