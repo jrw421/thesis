6465339db17be3180341546156a6010478af7e96
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-02-01 10:24:33 -0500

    adding contact functionality in progress

--- Server/passportConfig/passport-setup.js
+++ Server/passportConfig/passport-setup.js
@@ -23,7 +23,7 @@ passport.use(
     clientID: '958835359621-ar0pkshcuaba693ki10vaq1cc1j6qtk8.apps.googleusercontent.com',
     clientSecret: '4qDzcSsqkWieHEABXAf1XMpH'
   }, (accessToken, refreshToken, profile, done) => {
-    // console.log('accessToken is ', accessToken)
+    console.log('refresh is ', refreshToken)
 
     const body = {
       google_id: profile.id,
