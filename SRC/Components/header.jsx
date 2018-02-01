234826dcf2482519efa8564945a8fa6f1e745554
Evan Baker <3van90@gmail.com>
2018-02-01 12:23:53 -0500

    Fix navbar

--- SRC/Components/header.jsx
+++ SRC/Components/header.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import AppBar from 'material-ui/AppBar';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const tabStyle = {
+  paddingLeft: '12.5px',
+  paddingRight: '12.5px',
+  marginTop: '5px',
+  textAlign: 'center'
+}
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <AppBar
-          title="Host.ly"
-        />
-          <Link to="/dashboard">
-            Dashboard
-          </Link>
-          <div></div>
-          <Link to="/createEvent">
-            Create Event
-          </Link>
-          <a href="/auth/logout">Logout</a>
+        <AppBar title="Host.ly">
+          <Tabs onChange={this.changeTab}>
+            <Tab 
+            value={0} 
+            label="Dashboard"
+            containerElement={<Link to="/dashboard" />}
+            style={tabStyle}
+            />
+            <Tab 
+            value={1}
+            label="Create Event"
+            containerElement={<Link to="/createEvent" />}
+            style={tabStyle}
+            />
+            <Tab
+              value={2} 
+              label="Logout"
+              containerElement={<a href="/auth/logout"/>}
+              style={tabStyle}
+            />
+          </Tabs>
+        </AppBar>
       </div>
     )
   }
