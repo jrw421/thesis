f69f0d745a87c6e7714a5102f8ab2018abc13f2e
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-02-01 21:49:31 -0500

    contacts need to be passed to front end still

--- Server/passportConfig/passport-setup.js
+++ Server/passportConfig/passport-setup.js
@@ -37,7 +37,7 @@ passport.use(
     clientID: '958835359621-ar0pkshcuaba693ki10vaq1cc1j6qtk8.apps.googleusercontent.com',
     clientSecret: '4qDzcSsqkWieHEABXAf1XMpH'
   }, (accessToken, refreshToken, profile, done) => {
-
+    console.log('access token!!!!!!!!', accessToken)
     const body = {
       google_id: profile.id,
       name: profile.displayName,
