855e6b24cd5b447b67d8981dd17c0fcac3c790a6
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-02-03 15:23:41 -0500

    merging

--- main_src/Components/eventList.jsx
+++ main_src/Components/eventList.jsx
@@ -39,7 +39,6 @@ class EventList extends React.Component {
 
             return (
               <Card>
-                {/* <div>{item.description} </div> */}
 
                     {(this.props.events.length === 0) &&
                         <h3 style={{"textAlign": "center"}}>No events yet!</h3> }
@@ -49,7 +48,6 @@ class EventList extends React.Component {
                             <div onClick={() => this.props.handleEventClick(item)}>
                             <br></br>
                             <br></br>
-                            {/* <img style={{"height": "300px", "width": "300px", "textAlign": "center"}} className="img" src={item.img}></img> */}
                             <CardMedia
                                 overlay={<CardTitle title={item.name} subtitle={item.description} />}
                               >
@@ -66,21 +64,6 @@ class EventList extends React.Component {
                           </CardActions>
                         }
                     </div>
-
-
-
-                 {/* <CardMedia
-                      overlay={<CardTitle title={item.name} subtitle={item.description} />}
-                    >
-                      <img style={{"height":"400px", "width": "150px"}}
-                        src={item.img} alt="" />
-                    </CardMedia>
-                    {(this.props.events.length > 0) &&
-                    <CardActions>
-                      <FlatButton label="previous" onClick={this.changeEventBack.bind(this)}/>
-                      <FlatButton label="next" onClick={this.changeEvent.bind(this)}/>
-                    </CardActions>
-                    } */}
 
 
                 </Card>
