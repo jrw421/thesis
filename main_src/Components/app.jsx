6ad0d06cfd23d25482cd9bb720007010395a7b13
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-02-12 15:17:02 -0500

    pulling down

--- main_src/Components/app.jsx
+++ main_src/Components/app.jsx
@@ -12,11 +12,7 @@ import DashboardWithData from './dashboard';
 import EventPageWithData from './eventPage';
 import CreateEventWithMutations from './createEvent';
 import '../Styles/styles.scss';
-<<<<<<< HEAD
 import Loader from 'react-loader-spinner';
-=======
-import Loader from 'react-loader-spinner'
->>>>>>> added loader for intial load
 
 // import register from '../../swCheck';
 
