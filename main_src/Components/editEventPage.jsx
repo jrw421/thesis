cce6c89ad9d72e97b5ec64140190b3263cfe9d35
Caio Araujo Roper <cararoper@gmail.com>
2018-02-13 20:11:13 -0500

    Decouple item registery edit page from edit event page.

--- main_src/Components/editEventPage.jsx
+++ main_src/Components/editEventPage.jsx
@@ -4,7 +4,9 @@ import gql from 'graphql-tag';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+
 import ItemList from './itemList';
+
 
 class EditEventPage extends React.Component {
   constructor(props) {
@@ -15,9 +17,11 @@ class EditEventPage extends React.Component {
       date: this.props.event.date,
       time: this.props.event.time,
       description: this.props.event.description,
-      img: this.props.event.img
+      img: this.props.event.img,
+      showItems: false,
     }
     this.submitChanges = this.submitChanges.bind(this)
+    this.showItemList = this.showItemList.bind(this)
   }
 
   submitChanges() {
@@ -53,6 +57,10 @@ class EditEventPage extends React.Component {
     })
   }
 
+  showItemList() {
+    this.setState({showItems: !this.state.showItems})
+  }
+
  
 
   render() {
@@ -62,31 +70,50 @@ class EditEventPage extends React.Component {
   //   alt=""
   // />
 
+  
+
     let event = this.props.event;
 
     return(
-      <section className="edit-event">
+      <div className="edit-event">
         <div className="edit-event-grid">
           <div className="edit-event-background-img">
-          <div className="edit-event-inputs">
+
+
+          { !this.state.showItems ? (
+            <div>
+            <div className="edit-event-inputs">
           <h1>Edit Event</h1>
                 <TextField type="text" placeholder={event.name} className="input" secondary={true} onChange={(e) => this.setState({name: e.target.value})}></TextField>
                 <TextField type="text" placeholder={event.location} className="input" secondary={true} onChange={(e) => this.setState({location: e.target.value})}></TextField>
                 <TextField type="text" placeholder={event.date} className="input" secondary={true} onChange={(e) => this.setState({date: e.target.value})}></TextField>
                 <TextField type="text" placeholder={event.time} className="input" secondary={true} onChange={(e) => this.setState({time: e.target.value})}></TextField>
                 <TextField type="text" placeholder={event.description} className="input" secondary={true} onChange={(e) => this.setState({description: e.target.value})}></TextField>
-                <RaisedButton label="Submit Changes" primary={true} onClick={this.submitChanges} />
               </div>
-            <ul>
-              <ItemList 
+              <div className="edit-event-inputs-buttons">
+                <span>
+                <RaisedButton label="Submit Changes" primary={true} onClick={this.submitChanges} />
+                </span>
+                <span className="svg">
+                <svg onClick={this.showItemList} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.028 0v6.425l5.549 5.575-5.549 5.575v6.425l11.944-12z"/></svg>
+                </span>
+                </div>
+              </div>
+            ) : (
+              <div>
+              <ul>
+            <ItemList 
               event={this.props.event}
               currentUser={this.props.currentUser}
               currentlyEditing={this.props.currentlyEditing}
                 />
-            </ul>
+                </ul>
+                </div>
+                
+            )  }
           </div>
         </div>
-      </section>
+      </div>
     )
   }
 
