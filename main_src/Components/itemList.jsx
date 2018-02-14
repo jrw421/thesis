2bf83053d303896b9927eee669d1ed90b9a32ea0
Caio Araujo Roper <cararoper@gmail.com>
2018-02-14 10:55:04 -0500

    Change and resize delete button svg.

--- main_src/Components/itemList.jsx
+++ main_src/Components/itemList.jsx
@@ -51,7 +51,6 @@ class ItemList extends React.Component {
           }
         })
         .then(response => {
-          console.log('what we get back from mut', response);
           this.setState({ itemToAdd: '' }, () => {
             this.refreshItemList();
           });
@@ -73,10 +72,10 @@ class ItemList extends React.Component {
     }
 
     const deleteIcon = <svg xmlns="http://www.w3.org/2000/svg" 
-      width="24" 
-      height="24" 
-      viewBox="0 0 24 24">
-      <path d="M24 3.752l-4.423-3.752-7.771 9.039-7.647-9.008-4.159 4.278c2.285 2.885 5.284 5.903 8.362 8.708l-8.165 9.447 1.343 1.487c1.978-1.335 5.981-4.373 10.205-7.958 4.304 3.67 8.306 6.663 10.229 8.006l1.449-1.278-8.254-9.724c3.287-2.973 6.584-6.354 8.831-9.245z"/></svg>
+    width="12" 
+    height="12" 
+    viewBox="0 0 24 24">
+    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg>
 
     return this.props.currentlyEditing ? (
       <div className="item-list-two">
@@ -92,12 +91,14 @@ class ItemList extends React.Component {
               onKeyPress={this.addItem}
             />
           </li>
+          <div className="item-list-list">
           {this.props.itemsQuery.event.items.map(item => (
-            <li key={item.id}>
-              {item.name}
+            <li key={item.id} className="item-list-item">
+              <span>{item.name}</span>
               <span onClick={e => this.deleteItem(item.id, e)}>{deleteIcon}</span>
             </li>
           ))}
+          </div>
         </ul>
       </div>
     ) : (
