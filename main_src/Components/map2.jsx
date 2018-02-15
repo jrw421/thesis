de59bb255cf2df9285918da917e8433fce172a3a
Evan Baker <3van90@gmail.com>
2018-02-15 18:45:12 -0500

    Style map 2

--- main_src/Components/map2.jsx
+++ main_src/Components/map2.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import {withGoogleMap, GoogleMap, Marker, Map, InfoWindow} from 'react-google-maps';
+import ReactDOM from 'react-dom';
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  Map,
+  InfoWindow
+} from 'react-google-maps';
 import Modal from 'react-modal';
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
-import {GoogleApiWrapper} from 'google-maps-react'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from 'react-places-autocomplete';
+import { GoogleApiWrapper } from 'google-maps-react';
 import FlatButton from 'material-ui/FlatButton';
 
 class Map2 extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       currLocation: {},
@@ -16,121 +25,129 @@ class Map2 extends React.Component {
       latLng: [],
       isModalOpen: false,
       results: []
-    }
-    this.getResults = this.getResults.bind(this)
-    this.initialize = this.initialize.bind(this)
-    this.createMarkers = this.createMarkers.bind(this)
+    };
+    this.getResults = this.getResults.bind(this);
+    this.initialize = this.initialize.bind(this);
+    this.createMarkers = this.createMarkers.bind(this);
   }
 
   componentDidMount() {
-    this.initialize()
+    this.initialize();
   }
 
-    initialize() {
-      var eventLoc = new google.maps.LatLng(this.props.props.latLng);
+  initialize() {
+    var eventLoc = new google.maps.LatLng(this.props.props.latLng);
 
-      this.map = new google.maps.Map(document.getElementById('map'), {
-          center: eventLoc,
-          zoom: 35
-        });
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: eventLoc,
+      zoom: 35
+    });
 
-      var request = {
-        location: eventLoc,
-        radius: '500',
-        type: ['supermarket', 'convenience_store', 'liquor_store']
-      };
+    var request = {
+      location: eventLoc,
+      radius: '500',
+      type: ['supermarket', 'convenience_store', 'liquor_store']
+    };
 
-      let service = new google.maps.places.PlacesService(this.map);
-      service.nearbySearch(request, this.getResults);
-      this.map.setZoom(10)
+    let service = new google.maps.places.PlacesService(this.map);
+    service.nearbySearch(request, this.getResults);
+    this.map.setZoom(10);
+  }
+
+  async getResults(results, status) {
+    let res = await results;
+
+    if (res) {
+      this.setState({
+        results: res
+      });
     }
 
-     async getResults (results, status) {
-      let res = await results
-
-      if (res) {
-        this.setState({
-          results: res
-        })
-      }
-
-      for (var i = 0 ; i < res.length; i++) {
-        this.createMarkers(res[i])
-      }
+    for (var i = 0; i < res.length; i++) {
+      this.createMarkers(res[i]);
     }
+  }
 
+  createMarkers(place) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('map');
 
+    if (place.name === 'Brooklyn' || place.name === 'New York') {
+      return null;
+    }
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    var marker = new google.maps.Marker({
+      map: this.map,
+      icon: image,
+      // label: place.name,
+      title: place.name,
+      position: place.geometry.location
+    });
 
-      createMarkers(place) {
-        var bounds = new google.maps.LatLngBounds();
-        var placesList = document.getElementById('map');
+    let service = new google.maps.places.PlacesService(this.map);
 
-          if (place.name === 'Brooklyn' || place.name === 'New York') {
-            return null;
-          }
-          var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-          };
-          var marker = new google.maps.Marker({
-            map: this.map,
-            icon: image,
-            // label: place.name,
-            title: place.name,
-            position: place.geometry.location
-          });
+    const infoWindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', function() {
+      service.getDetails(place, function(result, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        infoWindow.setContent(
+          '<div><strong>' +
+            result.name +
+            '</strong><br>' +
+            '<p>' +
+            result.formatted_address +
+            '</p>' +
+            `<p><a href=${
+              result.url
+            }><strong>Get Directions</strong> </a></p>` +
+            '</div>'
+        );
 
-         let service = new google.maps.places.PlacesService(this.map);
+        infoWindow.open(map, marker);
+      });
+    });
 
-          const infoWindow = new google.maps.InfoWindow();
-          google.maps.event.addListener(marker, 'click', function() {
-            service.getDetails(place, function(result, status) {
-              console.log('what is result ', result)
-              if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                console.error(status);
-                return;
-              }
-              infoWindow.setContent('<div><strong>' + result.name + '</strong><br>' +
-                '<p>' + result.formatted_address + '</p>' +
-                `<p><a href=${result.url}><strong>Get Directions</strong> </a></p>` +
-                '</div>')
+    bounds.extend(place.geometry.location);
+    this.map.fitBounds(bounds);
+    this.map.setZoom(15);
+  }
 
-              infoWindow.open(map, marker);
-            });
-          });
+  render() {
+    const style = {
+      width: '100%',
+      height: '100%',
+      textAlign: 'center',
+      position: 'fixed'
+    };
 
-        bounds.extend(place.geometry.location);
-        this.map.fitBounds(bounds);
-        this.map.setZoom(15)
-      }
+    // const style = {
+    //   width: '100%',
+    //   height: '100%',
+    //   position: 'fixed',
+    //   left: '25%'
+    // };
+    const res = this.state.results;
 
-
-
-    render() {
-      const style = {
-        width: '50vw',
-        height: '40vh',
-        textAlign: 'center',
-        position: 'fixed',
-        left: '25%'
-      }
-      const res = this.state.results
-
-      if (!this.props.props.props.google) {
-        return <div>loading...</div>
-      }
-      const results = this.state.results
-      return (
-        <div>
-         <div id="map"  style={style}>
-           Loading...
-         </div>
-
+    if (!this.props.props.props.google) {
+      return <div>loading...</div>;
+    }
+    const results = this.state.results;
+    return (
+      <div style={{ width: '100%', height: '100%' }}>
+        <div id="map" style={style}>
+          Loading...
+        </div>
       </div>
-      )
+    );
   }
 }
 
